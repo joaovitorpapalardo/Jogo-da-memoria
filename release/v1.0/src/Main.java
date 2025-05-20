2813757780edd04b445866c8083a0c8675ce4581
João Vitor <134158096+joaovitorpapalardo@users.noreply.github.com>
2025-05-20 08:54:54 -0300

    Update Main.java

--- release/v1.0/src/Main.java
+++ release/v1.0/src/Main.java
@@ -1,1 +1,10 @@
-Main 1.0
+package v1;
+
+public class Main {
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            JogoMemoria jogo = new JogoMemoria();
+            jogo.setVisible(true);
+        });
+    }
+}
