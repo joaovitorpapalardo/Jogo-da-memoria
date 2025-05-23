25df647492a55985ed945e415e61dc1016c9891b
João Vitor <134158096+joaovitorpapalardo@users.noreply.github.com>
2025-05-23 18:13:42 -0300

    Update TelaRanking.java

--- release/v1.0/src/TelaRanking.java
+++ release/v1.0/src/TelaRanking.java
@@ -4,7 +4,19 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.List;
 
+/**
+ * Janela gráfica que exibe o ranking com os 10 melhores placares do jogo da memória.
+ * Os dados são carregados a partir do arquivo CSV gerenciado pela classe {@link Placares}.
+ * @author joaovitorpapalardo, joaovitorrodrigues, milenacamposm, nicolasbabo
+ * @version 1.0
+ * @since 1.0
+ */
 public class TelaRanking extends JFrame {
+    /**
+     * Construtor da janela de ranking.
+     * Inicializa a interface, define o título e exibe os dados dos 10 melhores jogadores
+     * em uma tabela com nome e número de tentativas.
+     */
     public TelaRanking() {
         setTitle("Top 10 Placares");
         setSize(300, 300);
