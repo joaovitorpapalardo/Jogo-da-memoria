7a5fb27731624b0774b9fcd30d53ac9d58084837
João Vitor <134158096+joaovitorpapalardo@users.noreply.github.com>
2025-05-23 18:13:09 -0300

    Update Main.java

--- release/v1.0/src/Main.java
+++ release/v1.0/src/Main.java
@@ -1,6 +1,18 @@
 package v1;
 
+/**
+ * Classe principal que inicia a aplicação do jogo da memória.
+ * Utiliza a thread de eventos do Swing para garantir que a interface gráfica
+ * seja criada de forma segura.
+ * @author joaovitorpapalardo, joaovitorrodrigues, milenacamposm, nicolasbabo
+ * @version 1.0
+ */
 public class Main {
+    /**
+     * Método principal que inicializa o jogo da memória.
+     *
+     * @param args argumentos da linha de comando (não utilizados)
+     */
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(() -> {
             JogoMemoria jogo = new JogoMemoria();
